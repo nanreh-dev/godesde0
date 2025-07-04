721bc0bb2c3de253f9b293dddd40914a6c1b52da
Hernan Camara <hernan.camara@metafar.io>
2025-07-04 08:43:28 -0300

    primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hcamara/godesde0
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/hcamara/godesde0/variables"
+)
+
+func main() {
+	variables.MostrarEnteros()
+}
